2cd60ca6e1a7408750a9e62e936eb2fefad26ef6
Mattia Manzati <manzati.mattia@gmail.com>
2018-01-30 18:09:58 +0100

    then/catch return future as well

--- src/Future.ts
+++ src/Future.ts
@@ -12,18 +12,17 @@ export class Future<T> {
 
     then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2>
     {
-        return this.value.then.apply(this.value, arguments)
+        return new Future(this.value.then.apply(this.value, [onfulfilled, onrejected].filter(i => !!i).map(f => action(f as any))))
     }
 
     catch<TResult2 = never>(onrejected: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null){
-        return this.value.catch.apply(this.value, arguments)
+        return new Future(this.value.catch.apply(this.value, [onrejected].filter(i => !!i).map(f => action(f as any))))
     }
 
     chain<R>(f: (value: T) => Future<R>) {
         return new Future<R>(
             schedule<R>((res, rej) => 
-                this.value
-                    .then(action(f))
+                this.then(f)
                     .then(res, rej))
         )
     }
@@ -31,10 +30,17 @@ export class Future<T> {
     map<R>(f: (a: T) => R){
         return new Future<R>(
             schedule<R>((res, rej) => 
-                this.value
-                    .then(action(f))
+                this.then(f)
                     .then(res, rej)
             ))
+    }
+
+    ap<B>(fab: Future<(a: T) => B>): Future<B> {
+        return new Future<B>(schedule<B>((res, rej) => 
+            all([fab, this])
+                .then(([f, a]: [(a: T) => B, T]) => f(a))
+                .then(res, rej)
+        ))
     }
 }
 
@@ -46,4 +52,11 @@ export function all<F1, F2, F3>(p: [Future<F1>, Future<F2>, Future<F3>]): Future
 export function all<F1, F2, F3, F4>(p: [Future<F1>, Future<F2>, Future<F3>, Future<F4>]): Future<[F1, F2, F3, F4]>
 {
     return new Future(Promise.all(p))
+}
+
+export function race<F1, F2>(p: [Future<F1>, Future<F2>]): Future<F1 | F2>
+export function race<F1, F2, F3>(p: [Future<F1>, Future<F2>, Future<F3>]): Future<F1 | F2 | F3>
+export function race<F1, F2, F3, F4>(p: [Future<F1>, Future<F2>, Future<F3>, Future<F4>]): Future<F1 | F2 | F3 | F4>
+{
+    return new Future(Promise.race(p))
 }
